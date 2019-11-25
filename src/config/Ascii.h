dfad9a4b43dadae1a7f3a6af72a21d6dbdb6dbf3
“bytrixcoin” <bytrixcoin@protonmail.com>
2019-11-25 23:56:39 +0200

    Initial commit

---
+++ src/config/Ascii.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+
+
+
+
+"	888888b.            888            d8b Y88b   d88P 	\n"
+"	888  "88b           888            Y8P  Y88b d88P  	\n"
+"	888  .88P           888                  Y88o88P   	\n"
+"	8888888K.  888  888 888888 888d888 888    Y888P    	\n"
+"	888  "Y88b 888  888 888    888P"   888    d888b    	\n"
+"	888    888 888  888 888    888     888   d88888b   	\n"
+"	888   d88P Y88b 888 Y88b.  888     888  d88P Y88b  	\n"
+"	8888888P"   "Y88888  "Y888 888     888 d88P   Y88b 	\n"
+"	                888                                	\n"
+"	           Y8b d88P                                	\n"
+"	            "Y88P"                                 	\n";
+
